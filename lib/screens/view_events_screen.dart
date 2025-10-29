7a16475bd506013e4594fc6fa54dd697bfe6fb8a
yaskho <yassineholberton@gmail.com>
2025-10-29 22:26:51 +0100

    view event modifications

--- lib/screens/view_events_screen.dart
+++ lib/screens/view_events_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:intl/intl.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class ViewEventsScreen extends StatelessWidget {
   const ViewEventsScreen({super.key});
